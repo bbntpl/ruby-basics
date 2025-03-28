da268b927d82bb19830fa4474c1c76011c9bd21b
bbntpl <beaver@bbntpl>
2025-03-27 23:31:14 -0400

    add has_swapped flag for bubble sort

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,22 +1,22 @@
 def bubble_sort(arr, level = 0)
   last_index_to_swap = arr.length - 1
+  has_swapped = false
+
   (last_index_to_swap - level).times do |i|
     next_i = i+1
     next_n = arr[next_i]
     n = arr[i]
 
     if n > next_n
+      has_swapped = true
       arr[i] = next_n
       arr[next_i] = n
     end
   end
 
-  return arr.length <= level ? arr : bubble_sort(arr, level+1)
-end
-
-def delete_by_index(arr, index)
-  arr.delete_at(index)
-  arr
+  return arr if arr.length <= level || !has_swapped 
+  
+  bubble_sort(arr, level+1)
 end
 
 puts bubble_sort([4, 3, 78, 2, 0, 2])
